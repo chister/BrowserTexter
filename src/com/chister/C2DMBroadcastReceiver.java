abb66abe532a5db0b2e2576d71762d1ef525a99d
Michael Chi <chister@gmail.com>
2011-12-01 14:31:53 -0800

    second commit

--- src/com/chister/C2DMBroadcastReceiver.java
+++ src/com/chister/C2DMBroadcastReceiver.java
@@ -1,8 +1,5 @@
 package com.chister;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -23,35 +20,11 @@ public class C2DMBroadcastReceiver extends BroadcastReceiver {
 	private void handleRegistration(Context context, Intent intent) {
 	    String registration_id = intent.getStringExtra("registration_id");
 	    
-		NotificationManager nm = 
-			(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-		CharSequence text = "Michael Chi";
-		Notification notification = new Notification(R.drawable.icon, text, System.currentTimeMillis());
-		
-		CharSequence contentTitle = "registration";
-		CharSequence contentText = registration_id;
-		Intent notificationIntent = new Intent(context, InitBrowserTexter.class);
-		PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
-
-		notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
-		//sendSMS(context, "3107094858", registration_id);
-		//nm.notify(1, notification);
-	    if (intent.getStringExtra("error") != null) {
-	        // Registration failed, should try again later.
-	    } else if (intent.getStringExtra("unregistered") != null) {
-	        // unregistration done, new messages from the authorized sender will be rejected
-	    } else if (registration_id != null) {
-	       // Send the registration ID to the 3rd party site that is sending the messages.
-	       // This should be done in a separate thread.
-	       // When done, remember that all registration is done.
-	    	HttpInterface.updateRegistrationId(context, registration_id);
-	    }
+	    HttpInterface.updateRegistrationId(context, registration_id);
 	}
 
 	private void sendSMS(Context context, String phoneNumber, String message)
     {        
-        //PendingIntent pi = PendingIntent.getActivity(context, 0, new Intent(context, InitBrowserTexter.class), 0);                
         SmsManager sms = SmsManager.getDefault();
         sms.sendTextMessage(phoneNumber, null, message, null, null);        
     }  
